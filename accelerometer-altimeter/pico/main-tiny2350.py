a3cf41d4944f1a85f9c33ebddea1448ed93af728
Sam Procter <sam.procter@gmail.com>
2024-11-28 16:49:22 -0500

    Begin accel config
    And fix nasty user bank bug :(

--- accelerometer-altimeter/pico/main-tiny2350.py
+++ accelerometer-altimeter/pico/main-tiny2350.py
@@ -44,14 +44,18 @@ shutdown_button = Signal(Pin(23, Pin.IN), invert = True)
 # Constants
 _ACCEL_ADDR = const(0x68) # Default ICM20649 I2C Addr: 104
 _REG_BANK_SEL = const(0x7F) # All banks
+_LP_CONFIG = const(0x05) # Bank 0, pg 39
 _PWR_MGMT_1 = const(0x06) # Bank 0, pg 39
-_TEMP_OUT_H = const(0x39) # Bank 0
+_PWR_MGMT_2 = const(0x07) # Bank 0, pg 40
 _ACCEL_XOUT_H = const(0x2D) # Bank 0
-_GYRO_CONFIG_1 = const(0x01) # Bank 2, pg 61
+_ODR_ALIGN_EN = const(0x09) # Bank 2, pg 65
+_ACCEL_SMPLRT_DIV_1 = const(0x10) # Bank 2, pg 65
+_ACCEL_SMPLRT_DIV_2 = const(0x11) # Bank 2, pg 65
 _ACCEL_CONFIG = const(0x14) # Bank 2, pg 66
+_ACCEL_CONFIG_2 = const(0x15) # Bank 2, pg 67
 
 _ALTI_ADDR = const(0x63) # Default ICP10124 I2C Addr: 99
-_CURR_BARO_PRESSURE = const(1011)
+_CURR_BARO_PRESSURE = const(1007)
 
 alti_calib_data = list()
 i2c = I2C(id=0, scl=13, sda=12, freq=400000)
@@ -59,47 +63,100 @@ i2c = I2C(id=0, scl=13, sda=12, freq=400000)
 # This process is described in IC10125 datasheet section 5.10 (pg 18)
 def read_alti_calib():
     # 0xC59500669C is the command to set up OTP read
-    i2c.writeto(_ALTI_ADDR, bytes.fromhex("C59500669C")) 
+    i2c.writeto(_ALTI_ADDR, b'\xC5\x95\x00\x66\x9C') 
     for i in range(4):
         # 0xC7F7 is the command to incrementally read out OTP
-        i2c.writeto(_ALTI_ADDR, bytes.fromhex("C7F7"))
+        i2c.writeto(_ALTI_ADDR, b'\xC7\xF7')
         temp_data = bytearray(3)
         i2c.readfrom_into(_ALTI_ADDR, temp_data)
         alti_calib_data.append(unpack(">h", temp_data[:2])[0])
     return alti_calib_data
 
 def initialize_accel():
-    # Turn off temperature sensor, use best available clock source
     write_byte = bytearray(1)
+
+    # Begin with Bank 0 configuration    
+    write_byte[0] = 0b00000000
+    i2c.writeto_mem(_ACCEL_ADDR, _REG_BANK_SEL, write_byte)
+
+    # Reset the device to default settings
+    write_byte[0] = 0b10000000
+    i2c.writeto_mem(_ACCEL_ADDR, _PWR_MGMT_1, write_byte)
+    time.sleep_ms(5)
+    while i2c.readfrom_mem(_ACCEL_ADDR, _PWR_MGMT_1, 1) == 128:
+        print("waiting")
+        time.sleep_ms(5)
+
+    # Turn low power off, temperature sensor off, use best available clock source
     write_byte[0] = 0b00001001
     i2c.writeto_mem(_ACCEL_ADDR, _PWR_MGMT_1, write_byte)
+    time.sleep_ms(30)
+
+    # Turn off the gyroscope, turn on the accelerometer
+    write_byte[0] = 0b00000111
+    i2c.writeto_mem(_ACCEL_ADDR, _PWR_MGMT_2, write_byte)
+
+    # Operate accelerometer in duty cycled mode
+    write_byte[0] = 0b0010000
+    i2c.writeto_mem(_ACCEL_ADDR, _LP_CONFIG, write_byte)
+
+    # Now on to Bank 2 configuration
+    write_byte[0] = 0b00100000
+    i2c.writeto_mem(_ACCEL_ADDR, _REG_BANK_SEL, write_byte)
+    time.sleep_ms(5)
+    
+    write_byte[0] = 0b00000001
+    i2c.writeto_mem(_ACCEL_ADDR, _ODR_ALIGN_EN, write_byte)
+
+    # Sample rate = 10, rate_hz = 1125/(1 + rate) = 1125/11 = 112.5
+    # Split across two bytes, 12 bits
+    write_byte[0] = 0b00000000 # MSB
+    i2c.writeto_mem(_ACCEL_ADDR, _ACCEL_SMPLRT_DIV_1, write_byte)
+    write_byte[0] = 0b00001010 # LSB
+    i2c.writeto_mem(_ACCEL_ADDR, _ACCEL_SMPLRT_DIV_2, write_byte)
+    
+    # Low pass filter = 3, Accelerometer Full Scale 30g, DLPF Enabled
+    write_byte[0] = 0b00011111
+    i2c.writeto_mem(_ACCEL_ADDR, _ACCEL_CONFIG, write_byte)
+
+    # Average 4 samples (would be 1, but we're using the DLPF)
+    write_byte[0] = 0b00000001
+    i2c.writeto_mem(_ACCEL_ADDR, _ACCEL_CONFIG_2, write_byte)
+
+    #Back to Bank 0 
+    write_byte[0] = 0b00000000
+    i2c.writeto_mem(_ACCEL_ADDR, _REG_BANK_SEL, write_byte)
+
+    time.sleep_ms(100)
+    # Expected power consumption: 120uA
+
 
 def print_accel_data(reading : bytearray):
     raw_accel_x, raw_accel_y, raw_accel_z = unpack(">hhh", reading[:6])
-    raw_gyro_x, raw_gyro_y, raw_gyro_z = unpack(">hhh", reading[6:12])
-    
-    # Accelerometer "sensitivity scale factors" on page 13
-    accel_x = (raw_accel_x / 8192)
-    accel_y = (raw_accel_y / 8192)
-    accel_z = (raw_accel_z / 8192)
+    # raw_gyro_x, raw_gyro_y, raw_gyro_z = unpack(">hhh", reading[6:12])
 
-    # Gyroscope "sensitivity scale factors" on page 12
-    gyro_x = (raw_gyro_x / 65.5)
-    gyro_y = (raw_gyro_y / 65.5)
-    gyro_z = (raw_gyro_z / 65.5)
+    # Accelerometer "sensitivity scale factors" on page 13
+    accel_x = (raw_accel_x / 1024)
+    accel_y = (raw_accel_y / 1024)
+    accel_z = (raw_accel_z / 1024)
+
+    # # Gyroscope "sensitivity scale factors" on page 12
+    # gyro_x = (raw_gyro_x / 65.5)
+    # gyro_y = (raw_gyro_y / 65.5)
+    # gyro_z = (raw_gyro_z / 65.5)
 
     # raw_temp = unpack(">h", reading[12:14])[0]
     # temp_c = (raw_temp / 333.87) + 21.0
     # temp_f = temp_c * 1.8 + 32
     print("Accel (x, y, z):", accel_x, accel_y, accel_z)
-    print("Gyro (x, y, z):", gyro_x, gyro_y, gyro_z)
+    # print("Gyro (x, y, z):", gyro_x, gyro_y, gyro_z)
     # print("Temp (Accel):", temp_f)
 
 def print_alti_data(reading : bytearray):
     raw_temp = unpack(">h", reading[:2])[0]
     temp_c = -45 + ((175/(2 ** 16)) * raw_temp)
     temp_f = temp_c * 1.8 + 32
-    print("Temp (Alti):", temp_f)
+    print("Temp (f):", temp_f)
 
     # reading[2], reading[5], and reading[9] are CRC bytes, and reading [7] 
     # "must be disregarded" Section 5.5, pg 15 of the datasheet
@@ -125,7 +182,7 @@ def print_alti_data(reading : bytearray):
     B = (p_Pa[0] - A) * (s1 + C)
     pressure_pa = A + B / (C + raw_press)
     altitude = (1-(((pressure_pa/100)/_CURR_BARO_PRESSURE) ** .190284)) * 145366.45
-    print("Pressure (Pa): ", pressure_pa)
+    # print("Pressure (Pa): ", pressure_pa)
     print("Altitude (ft): ", altitude)
 
 def init():
@@ -133,18 +190,22 @@ def init():
     read_alti_calib()
 
 def main_portion():
-    # Read from the accelerometer    
-    temp_data = bytearray(12)
-    i2c.readfrom_mem_into(_ACCEL_ADDR, _ACCEL_XOUT_H, temp_data)
-    print_accel_data(temp_data)
+    for i in range(2):
+        # Read from the accelerometer
+        temp_data = bytearray(6)
+        i2c.readfrom_mem_into(_ACCEL_ADDR, _ACCEL_XOUT_H, temp_data)
+        print_accel_data(temp_data)
 
-    # Read from altimeter
-    temp_data_2 = bytearray(9)
-    i2c.writeto(_ALTI_ADDR, bytes.fromhex("609C")) # Request Low Power, Temp 1st
-    time.sleep_ms(2)
-    i2c.readfrom_into(_ALTI_ADDR, temp_data_2)
-    print_alti_data(temp_data_2)
+        # Read from altimeter
+        temp_data_2 = bytearray(9)
+        i2c.writeto(_ALTI_ADDR, bytes.fromhex("609C")) # Request Low Power, Temp 1st
+        time.sleep_ms(3)
+        i2c.readfrom_into(_ALTI_ADDR, temp_data_2)
+        print_alti_data(temp_data_2)
 
+        print("")
+        
+        time.sleep_ms(100)
 
 # red_led.off()
 # grn_led.off()
