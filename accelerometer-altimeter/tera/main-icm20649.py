39e33a68cb27e7ab24537146fcfaf4239e98f3d6
Sam Procter <sam.procter@gmail.com>
2024-12-12 18:24:07 -0500

    Add timestamps for accelerometer data

--- accelerometer-altimeter/tera/main-icm20649.py
+++ accelerometer-altimeter/tera/main-icm20649.py
@@ -1,18 +1,25 @@
 from struct import unpack
 from io import open
 from os import chdir, scandir, listdir, DirEntry
+from more_itertools import peekable
 # TODO: import filters from accelerometer
 
 x_err = 0.029050755
 y_err = -0.008103238
 z_err = 0.044206185
 
-def decode_accel_data(accel_reading : bytes):
+def accel_timestamps(entries):
+    cur_timestamp, entry = 0, 0
+    while entry < entries:
+        yield cur_timestamp
+        cur_timestamp += 1/(1125/11)
+
+def decode_accel_data(accel_reading : bytes, timestamp : float):
     raw_accel_x, raw_accel_y, raw_accel_z = unpack(">hhh", accel_reading)
     accel_x = (raw_accel_x / 1024) - x_err
     accel_y = (raw_accel_y / 1024) - y_err
     accel_z = (raw_accel_z / 1024) - z_err
-    return (accel_x, accel_y, accel_z)
+    return (timestamp, accel_x, accel_y, accel_z)
 
 # accel_data = list() 
 accel_data = bytes()
@@ -33,10 +40,11 @@ for launch in scandir():
                     alti_data.append(bytes)
     chdir('..')
     i = 0
+    accel_timestamp = peekable(accel_timestamps(len(accel_data)//6))
     with open('launch'+launch.name+'.csv', 'wt') as f:
         #f.write("acc_x, acc_y, acc_z, acc_temp_f, gyro_x, gyro_y, gyro_z, roll, pitch, alti_temp_f, alti_ft, humid_pct\n")
-        f.write("acc_x, acc_y, acc_z\n")
+        f.write("time, acc_x, acc_y, acc_z\n")
         for i in range(len(accel_data)//6):
-            f.write(','.join(str(x) for x in decode_accel_data(accel_data[i * 6 : (i + 1) * 6])) + "\n")
+            f.write(','.join(str(x) for x in decode_accel_data(accel_data[i * 6 : (i + 1) * 6], next(accel_timestamp))) + "\n")
             i += 1
     alti_data.clear()
