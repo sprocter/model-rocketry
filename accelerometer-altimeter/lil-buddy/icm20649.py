289e30b9b20be59bb99d8961a357e74cfae2f4b3
Sam Procter <sam.procter@gmail.com>
2025-12-21 22:35:56 -0500

    Add ICM20649 driver

---
+++ accelerometer-altimeter/lil-buddy/icm20649.py
@@ -0,0 +1,153 @@
+"""A very simple driver for the ICM20649 Accelerometer and Gyroscope
+
+--------------------------------------------------------------------------------
+Copyright (C) 2025 Sam Procter
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+--------------------------------------------------------------------------------
+"""
+
+from machine import I2C
+from struct import unpack
+import time
+
+
+_ICM20649_ADDR = const(0x68)
+
+## Bank 0
+_REG_CHIPID = const(0x00)  # Datasheet pg 38, 8.1.1
+_REG_PWR_MGMT_1 = const(0x06)  # Datasheet pg 39, 8.1.4
+_REG_PWR_MGMT_2 = const(0x07)  # Datasheet pg 39, 8.1.5
+_REG_ACCEL_XOUT_H = const(0x2D)  # Datasheet pg 44, 8.1.17
+_REG_BANK_SEL = const(0x7F)  # Datasheet pg 56, 8.1.65
+
+## Bank 2
+_REG_GYRO_SMPLRT_DIV = const(0x00)  # pg 61, 8.3.1
+_REG_GYRO_CONFIG_1 = const(0x01)  # pg 61, 8.3.1
+_REG_ACCEL_SMPLRT_DIV_2 = const(0x11)  # pg 65, 8.3.12
+_REG_ACCEL_CONFIG = const(0x14)  # pg 66, 8.3.15
+_REG_TEMP_CONFIG = const(0x53)  # pg 69, 8.3.18
+
+_EXPECTED_DEVICE_ID = const(0xE1)  # Datasheet pg 38, 8.1.1
+
+_GYRO_SENSITIVITY = const(8.2)  # Datasheet pg 12, 3.1
+_ACCEL_SENSITIVITY = const(1024)  # Datasheet pg 13, 3.2
+
+_ACC_X_ERR = const(0.0057441408)
+_ACC_Y_ERR = const(-0.01940332)
+_ACC_Z_ERR = const(0.03321874)
+_GYRO_X_ERR = const(-0.53658104)
+_GYRO_Y_ERR = const(1.4446298)
+_GYRO_Z_ERR = const(-0.43841124)
+
+_TEMP_SENSITIVITY = const(333.87)  # Datasheet pg 15, 3.3.2
+_TEMP_OFFSET = const(0)  # Datasheet pg 15, 3.3.2
+
+
+class ICM20649:
+
+    def __init__(self, i2c: I2C) -> None:
+        self.i2c = i2c
+        self.buffer = bytearray(14)
+        self.addr = _ICM20649_ADDR
+
+    def initialize(self) -> None:
+        if _ICM20649_ADDR not in self.i2c.scan():
+            raise OSError(f"ICM20649 not found at {_ICM20649_ADDR}")
+        actual_device_id = unpack(
+            "<B", self.i2c.readfrom_mem(_ICM20649_ADDR, _REG_CHIPID, 1)
+        )[0]
+        if actual_device_id != _EXPECTED_DEVICE_ID:
+            raise OSError(f"ICM20649 has incorrect device id {actual_device_id}")
+
+        # Reset the device
+        self.i2c.writeto_mem(_ICM20649_ADDR, _REG_PWR_MGMT_1, b"\x80")  
+
+        time.sleep_ms(5) # Give the device time to do its reset
+
+        # Don't reset the device, wake it up from sleep, don't enable low-power
+        # Write 0 to reserve (😰), enable temperature sensor,
+        # auto select best clock source
+        # 0 0 0 0 001
+        self.i2c.writeto_mem(_ICM20649_ADDR, _REG_PWR_MGMT_1, b"\x01")
+
+        # Write 00 to reserved spot, enable accelerometer, enable gyro
+        # 00 000 000
+        self.i2c.writeto_mem(_ICM20649_ADDR, _REG_PWR_MGMT_2, b"\x00")
+
+        time.sleep_ms(50) # Allow gyro and accelerometer to wake up
+
+        # Switch to bank 2
+        # Bits 7, 6, 3, 2, 1 and 0 are reserved
+        # 00 10 0000
+        self.i2c.writeto_mem(_ICM20649_ADDR, _REG_BANK_SEL, b"\x20")
+
+        # Sample rate = 1.1kHz/(1+ X), X = 10, ODR = 102.3Hz
+        # 00001010
+        self.i2c.writeto_mem(_ICM20649_ADDR, _REG_GYRO_SMPLRT_DIV, b"\x0a")
+
+        # Bits 7 and 6 are reserved, gyro DLPF is set to 3 (?? I don't get this)
+        # Gyroscope is in full scale, enable gyro DLPF
+        # 00 011 11 1
+        self.i2c.writeto_mem(_ICM20649_ADDR, _REG_GYRO_CONFIG_1, b"\x1F")
+
+        # Sample rate = 1.1kHz/(1+X), X = 10, ODR = 102.3Hz
+        # 00001010
+        self.i2c.writeto_mem(_ICM20649_ADDR, _REG_ACCEL_SMPLRT_DIV_2, b"\x0a")
+
+        # Bits 7 and 6 are reserved, accel DLPF is set to 3 (? I don't get this)
+        # Accelerometer is in full scale, enable accel DLPF
+        # 00 011 11 1
+        self.i2c.writeto_mem(_ICM20649_ADDR, _REG_ACCEL_CONFIG, b"\x1F")
+
+        # Does a temperature sensor need a low pass filter? This seems weird
+        # 00000 011
+        self.i2c.writeto_mem(_ICM20649_ADDR, _REG_TEMP_CONFIG, b"\x03")
+
+        # Back to bank 0
+        # Bits 7, 6, 3, 2, 1 and 0 are reserved
+        # 00 00 0000
+        self.i2c.writeto_mem(_ICM20649_ADDR, _REG_BANK_SEL, b"\x00")
+
+        time.sleep_ms(50) # Let the low-pass filters "warm up"
+
+    def read_raw(self) -> None:
+        self.i2c.readfrom_mem_into(_ICM20649_ADDR, _REG_ACCEL_XOUT_H, self.buffer)
+
+        # self.i2c.writeto_mem(_ICM20649_ADDR, _REG_BANK_SEL, b"\x20")
+        # print(bin(int.from_bytes(self.i2c.readfrom_mem(_ICM20649_ADDR, _REG_ACCEL_CONFIG, 1))))
+        # self.i2c.writeto_mem(_ICM20649_ADDR, _REG_BANK_SEL, b"\x00")
+
+    def decode_reading(
+        self, reading: bytearray
+    ) -> tuple[float, float, float, float, float, float, float]:
+        unpacked_reading = unpack(">hhhhhhh", reading)
+        return (
+            unpacked_reading[0] / _ACCEL_SENSITIVITY - _ACC_X_ERR,
+            unpacked_reading[1] / _ACCEL_SENSITIVITY - _ACC_Y_ERR,
+            unpacked_reading[2] / _ACCEL_SENSITIVITY - _ACC_Z_ERR,
+            unpacked_reading[3] / _GYRO_SENSITIVITY - _GYRO_X_ERR,
+            unpacked_reading[4] / _GYRO_SENSITIVITY - _GYRO_Y_ERR,
+            unpacked_reading[5] / _GYRO_SENSITIVITY - _GYRO_Z_ERR,
+            (unpacked_reading[6] - _TEMP_OFFSET) / _TEMP_SENSITIVITY + 21,
+        )
+
+
+accel = ICM20649(I2C(scl=9, sda=8))
+accel.initialize()
+readings = bytearray(70)
+
+for i in range(5):
+    start = time.ticks_us()
+    accel.read_raw()
+    readings[i * 14 : (i+1) * 14] = accel.buffer
+    read_time = time.ticks_diff(time.ticks_us(), start)
+    print(f"Reading took {read_time}us")
+    time.sleep_ms(10)
+
+for i in range(5):
+    print(accel.decode_reading(readings[i * 14 : (i+1)*14]))
--- accelerometer-altimeter/utilities.py
+++ accelerometer-altimeter/utilities.py
@@ -44,7 +44,7 @@ def print_filesystem_space():
     print("Free : {:,} bytes, {:,} KB, {} MB".format(free, free / KB, free / MB))
 
 
-def print_accel_offsets():
+def print_adxl375_offsets():
     from adxl375 import ADXL375
 
     i2c = I2C(sda=41, scl=40)
@@ -68,5 +68,81 @@ def print_accel_offsets():
     print("_Y_ERR = ", sum(ys) / len(ys))
     print("_Z_ERR = ", sum(zs) / len(zs) - 1)
 
+def print_icm20649_offsets():
+    from icm20649 import ICM20649
 
-reset_filesystem()
+    i2c = I2C(scl=9, sda=8)
+    accelerometer = ICM20649(i2c)
+    accelerometer.initialize()
+    acc_xs, acc_ys, acc_zs = [], [], []
+    gyro_xs, gyro_ys, gyro_zs = [], [], []
+    print("Please ensure X, Y, and Z ERR constants are set to 0 in the ICM20649 Driver")
+    print("Set the device face-up on a flat surface and hold it still.")
+    print("Calibration begins in five seconds.")
+    time.sleep(5)
+    print("Calibration beginning now, it will take 10 seconds...")
+    for _ in range(1000):
+        accelerometer.read_raw()
+        accel_reading = accelerometer.decode_reading(accelerometer.buffer)
+        acc_xs.append(accel_reading[0])
+        acc_ys.append(accel_reading[1])
+        acc_zs.append(accel_reading[2])
+        gyro_xs.append(accel_reading[3])
+        gyro_ys.append(accel_reading[4])
+        gyro_zs.append(accel_reading[5])
+        time.sleep_ms(10)
+
+    print("_ACC_X_ERR = ", sum(acc_xs) / len(acc_xs))
+    print("_ACC_Y_ERR = ", sum(acc_ys) / len(acc_ys))
+    print("_ACC_Z_ERR = ", sum(acc_zs) / len(acc_zs) - 1)
+    print("_GYRO_X_ERR = ", sum(gyro_xs) / len(gyro_xs))
+    print("_GYRO_Y_ERR = ", sum(gyro_ys) / len(gyro_ys))
+    print("_GYRO_Z_ERR = ", sum(gyro_zs) / len(gyro_zs))
+
+def kalman_test():
+    from ulab import numpy as np
+    from kalman import KalmanFilter
+    from machine import freq
+
+    machine.freq(240000000)
+    print(f"CPU Frequency: {machine.freq()}")
+
+    init_timestamp = time.ticks_us()
+
+    F = np.array([[1, 1], [0, 1]])
+    B = np.array([[0.5], [1]])
+    H = np.array([[1, 0]])
+    Q = np.array([[1, 0], [0, 1]])
+    R = np.array([[1]])
+
+    x0 = np.array([[0], [1]])
+    P0 = np.array([[1, 0], [0, 1]])
+
+    kf = KalmanFilter(F, B, H, Q, R, x0, P0)
+
+    u = np.array([[1]])
+    z = np.array([[1]])
+
+    setup_timestamp = time.ticks_us()
+
+    predicted_state = kf.predict(u)
+    # print("Predicted state:\n", predicted_state)
+
+    prediction_timestamp = time.ticks_us()
+
+    updated_state = kf.update(z)
+    # print("Updated state:\n", updated_state)
+
+    update_timestamp = time.ticks_us()
+
+    setup_time = time.ticks_diff(setup_timestamp, init_timestamp)    
+    prediction_time = time.ticks_diff(prediction_timestamp, setup_timestamp)
+    update_time = time.ticks_diff(update_timestamp, prediction_timestamp)
+    
+    print(f"Setup Time: {setup_time}")
+    print(f"Prediction Time: {prediction_time}")
+    print(f"Update Time: {update_time}")
+
+
+
+print_icm20649_offsets()
