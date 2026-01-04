0955fc119687d3f5efdb639b7aafe1bf3a8d9b8d
Sam Procter <sam.procter@gmail.com>
2026-01-04 16:05:28 -0500

    Add magnetometer calibration

--- accelerometer-altimeter/lil-buddy/mmc5983ma.py
+++ accelerometer-altimeter/lil-buddy/mmc5983ma.py
@@ -21,9 +21,9 @@ _MMC5983MA_ADDR = const(0x30)
 
 _MMC5983MA_CHIPID = const(0x2F)  # pg 16, Product ID1
 _MMC5983MA_XOUT0 = const(0x00)  # pg 13, Register Map
-_MMC5983MA_CTRL0 = const(0x09) # pg 14
-_MMC5983MA_CTRL1 = const(0x0A) # pg 15
-_MMC5983MA_CTRL2 = const(0x0B) # pg 15
+_MMC5983MA_CTRL0 = const(0x09)  # pg 14
+_MMC5983MA_CTRL1 = const(0x0A)  # pg 15
+_MMC5983MA_CTRL2 = const(0x0B)  # pg 15
 
 _EXPECTED_DEVICE_ID = const(48)  # pg 16, Product ID1
 
@@ -32,43 +32,54 @@ class MMC5983MA:
 
     def __init__(self, i2c: I2C) -> None:
         self.i2c = i2c
-        self.buffer = bytearray(7) # Three 18 bit floats split across 7 bytes :|
+        self.buffer = bytearray(7)  # Three 18 bit floats split across 7 bytes :|
         self.addr = _MMC5983MA_ADDR
 
-    def _calculate_offsets(self) -> None:
-        """Calculate the device's offsets and store them
-        
-        This implements the procedure (described on pg 18 of the datasheet) for calculating the three axes' offsets. It modifies the _x|y|z_offset variables, which are used in the decode_reading function.
-        """
+        # "Local" offsets, set using the chip's SET and RESET functionality
         self._x_offset = 0
         self._y_offset = 0
         self._z_offset = 0
 
+        # "Hard Iron" Offsets, set using the calibration function
+        self._x_hi_offset = 0
+        self._y_hi_offset = 0
+        self._z_hi_offset = 0
+
+        # "Soft Iron" Offsets, set using the calibration function
+        self._x_si_offset = 1
+        self._y_si_offset = 1
+        self._z_si_offset = 1
+
+    def _calculate_offsets(self) -> None:
+        """Calculate the device's local offsets and store them
+
+        This implements the procedure (described on pg 18 of the datasheet) for calculating the three axes' offsets. It modifies the _x|y|z_offset variables, which are used in the decode_reading function.
+        """
         # "Set" operation
         # 0 0 0 0 1 0 0 0
         self.i2c.writeto_mem(_MMC5983MA_ADDR, _MMC5983MA_CTRL0, b"\x08")
-        time.sleep_us(1) # Datasheet says 500ns, we'll give a little extra
+        time.sleep_us(1)  # Datasheet says 500ns, we'll give a little extra
 
-        # Trigger reading 
+        # Trigger reading
         self.i2c.writeto_mem(_MMC5983MA_ADDR, _MMC5983MA_CTRL0, b"\x01")
         time.sleep_ms(10)
         self.read_raw()
         (x1, y1, z1) = self.decode_reading(self.buffer)
-        
+
         # "Reset" operation
         # 0 0 0 1 0 0 0 0
         self.i2c.writeto_mem(_MMC5983MA_ADDR, _MMC5983MA_CTRL0, b"\x10")
-        time.sleep_us(1) # Datasheet says 500ns, we'll give a little extra
+        time.sleep_us(1)  # Datasheet says 500ns, we'll give a little extra
 
-        # Trigger reading 
+        # Trigger reading
         self.i2c.writeto_mem(_MMC5983MA_ADDR, _MMC5983MA_CTRL0, b"\x01")
         time.sleep_ms(10)
         self.read_raw()
         (x2, y2, z2) = self.decode_reading(self.buffer)
 
-        self._x_offset = (x1 + x2)/2
-        self._y_offset = (y1 + y2)/2
-        self._z_offset = (z1 + z2)/2
+        self._x_offset = (x1 + x2) / 2
+        self._y_offset = (y1 + y2) / 2
+        self._z_offset = (z1 + z2) / 2
 
     def initialize(self) -> None:
         if _MMC5983MA_ADDR not in self.i2c.scan():
@@ -82,27 +93,70 @@ class MMC5983MA:
         # Reset the chip, take measurements for 8ms (100Hz B/W)
         # 1 00 00 0 0 0
         self.i2c.writeto_mem(_MMC5983MA_ADDR, _MMC5983MA_CTRL1, b"\x80")
-        time.sleep_ms(20) # Datasheet says power on time is 10ms, we'll give a little extra
+        # Datasheet says power on time is 10ms, we'll give a little extra
+        time.sleep_ms(20)
 
         self._calculate_offsets()
-        
+
         # Enable Auto Set/Reset
         # 0 0 1 0 0 0 0 0
-        # This is a neat feature but it 1) flips the signs from what I get from 
-        # a reference implementation and 2) requires re-establishing the 
+        # This is a neat feature but it 1) flips the signs from what I get from
+        # a reference implementation and 2) requires re-establishing the
         # offsets. So I don't know how to use it in my time-constrained setup
         # self.i2c.writeto_mem(_MMC5983MA_ADDR, _MMC5983MA_CTRL0, b"\x20")
 
         # Enable periodic settings, set every time*, enable continuous mode, Continuous frequency = 50hz
-        # * Since we're not using auto set / reset, I don't believe this affects anything. 
+        # * Since we're not using auto set / reset, I don't believe this affects anything.
         # 1 000 1 100
-        self.i2c.writeto_mem(_MMC5983MA_ADDR, _MMC5983MA_CTRL2, b"\x8C")
+        self.i2c.writeto_mem(_MMC5983MA_ADDR, _MMC5983MA_CTRL2, b"\x8c")
 
-        time.sleep_ms(10) # Block until first reading can be taken, they take 8ms
+        time.sleep_ms(10)  # Block until first reading can be taken, they take 8ms
 
+    def calibrate(self, duration=10):
+        """Attempt to calculate hard-iron and soft-iron distortion in order to compensate for it.
+
+        This takes samples over the user-specified number of seconds (default 10) and then does some math to figure out hard and soft iron distortion. Offsets / scale factors to compensate for that distortion are then stored in class variables and used to adjust future readings.
+
+        Note that the soft iron distortion is a "good enough" implementation that does not do an actual ellipsoid fit.
+
+        :param int duration: How long to run the calibration for in seconds
+
+        """
+        start_ts = time.ticks_ms()
+
+        x_min = y_min = z_min = 999999999
+        x_max = y_max = z_max = -999999999
+
+        while time.ticks_diff(time.ticks_ms(), start_ts) < duration * 1000:
+            self.read_raw()
+            (x, y, z) = self.decode_reading(self.buffer)
+            if x < x_min:
+                x_min = x
+            elif x > x_max:
+                x_max = x
+            if y < y_min:
+                y_min = y
+            elif y > y_max:
+                y_max = y
+            if z < z_min:
+                z_min = z
+            elif z > z_max:
+                z_max = z
+            time.sleep_ms(20)
+
+        # "Hard iron" distortion
+        self._x_hi_offset = (x_min + x_max) / 2
+        self._y_hi_offset = (y_min + y_max) / 2
+        self._z_hi_offset = (z_min + z_max) / 2
+
+        # "Soft iron" distortion -- uses scale biases, would be more accurate with an ellipsoid fit. See https://www.appelsiini.net/2018/calibrate-magnetometer/
+        avg_hi_offset = (self._x_hi_offset + self._y_hi_offset + self._z_hi_offset) / 3
+        self._x_si_offset = avg_hi_offset / self._x_hi_offset
+        self._y_si_offset = avg_hi_offset / self._y_hi_offset
+        self._z_si_offset = avg_hi_offset / self._z_hi_offset
 
     def read_raw(self) -> None:
-        if(self.i2c.readfrom_mem(_MMC5983MA_ADDR, 0x08, 1)[0] & 0x01 != 1):
+        if self.i2c.readfrom_mem(_MMC5983MA_ADDR, 0x08, 1)[0] & 0x01 != 1:
             # This should never happen, if it does make sure you aren't reading
             # faster than the measurement frequency
             print("MEASUREMENT ERROR: MEASUREMENT TAKEN BUT NOT READY")
@@ -112,7 +166,7 @@ class MMC5983MA:
         x_raw = reading[0] << 10 | reading[1] << 2 | reading[6] >> 6
         y_raw = reading[2] << 10 | reading[3] << 2 | ((reading[6] >> 4) & 0x03)
         z_raw = reading[4] << 10 | reading[5] << 2 | ((reading[6] >> 2) & 0x03)
-        x_adj = x_raw - self._x_offset
-        y_adj = y_raw - self._y_offset
-        z_adj = z_raw - self._z_offset
+        x_adj = (x_raw - self._x_offset - self._x_hi_offset) * self._x_si_offset
+        y_adj = (y_raw - self._y_offset - self._y_hi_offset) * self._y_si_offset
+        z_adj = (z_raw - self._z_offset - self._z_hi_offset) * self._z_si_offset
         return (x_adj, y_adj, z_adj)
