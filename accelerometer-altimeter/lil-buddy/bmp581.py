ff87e6aec14506d663b388fee7358032e71a940b
Sam Procter <sam.procter@gmail.com>
2026-01-08 21:27:26 -0500

    Update BMP581 driver to report temp and follow new style

--- accelerometer-altimeter/lil-buddy/bmp581.py
+++ accelerometer-altimeter/lil-buddy/bmp581.py
@@ -21,7 +21,7 @@ _BMP581_ADDR = const(0x47)
 # Page and section numbers refer to the datasheet
 
 _BMP581_CHIPID = const(0x01)  # pg 51, 7.1
-_BMP581_PRESS_XLSB = const(0x20)  # pg 56, 7.14
+_BMP581_TEMP_XLSB = const(0x1D)  # pg 56, 7.14
 _BMP581_DSP_IIR_CFG = const(0x31)  # pg 62, 7.28
 _BMP581_OSR_CONFIG = const(0x36)  # pg 64, 7.32
 _BMP581_ODR_CONFIG = const(0x37)  # pg 64-65, 7.33
@@ -34,7 +34,7 @@ class BMP581:
 
     def __init__(self, i2c: I2C) -> None:
         self.i2c = i2c
-        self.buffer = bytearray(3)
+        self.buffer = bytearray(6)
         self.addr = _BMP581_ADDR
 
     def initialize(self) -> None:
@@ -46,30 +46,36 @@ class BMP581:
         if actual_device_id != _EXPECTED_DEVICE_ID:
             raise OSError(f"BMP581 has incorrect device id {actual_device_id}")
 
-        # is "non-stop" the same as continuous? I think so?
-        # disable deep standby, ODR = 25.005hz, "non-stop" mode.
-        # 1 10100 11
-        self.i2c.writeto_mem(_BMP581_ADDR, _BMP581_ODR_CONFIG, b"\xd3")
+        # disable deep standby, ODR = 50.056hz, "normal" mode.
+        # 1 01111 01
+        self.i2c.writeto_mem(_BMP581_ADDR, _BMP581_ODR_CONFIG, b"\xBD")
         time.sleep_ms(10)
 
         # reserved, pressure iir = 2 / coeff 3, do not use temp iir filter
         # 00 010 000
         self.i2c.writeto_mem(_BMP581_ADDR, _BMP581_DSP_IIR_CFG, b"\x10")
 
-        # Reserved, enable pressure readings, pressure osr = 32x, temp osr = 2x
-        # 0 1 101 001
-        self.i2c.writeto_mem(_BMP581_ADDR, _BMP581_OSR_CONFIG, b"\x69")
+        # Reserved, enable pressure readings, pressure osr = 16x, temp osr = 2x
+        # 0 1 100 001
+        self.i2c.writeto_mem(_BMP581_ADDR, _BMP581_OSR_CONFIG, b"\x61")
         time.sleep_ms(10)
 
     def read_raw(self) -> None:
-        self.i2c.readfrom_mem_into(_BMP581_ADDR, _BMP581_PRESS_XLSB, self.buffer)
+        self.i2c.readfrom_mem_into(_BMP581_ADDR, _BMP581_TEMP_XLSB, self.buffer)
 
-    def decode_reading(self, reading: bytearray) -> float:
-        pressure_pa = (reading[2] << 16 | reading[1] << 8 | reading[0]) >> 6
+    def decode_reading(self, reading: bytearray) -> tuple[float, float]:
+        return (self.decode_alti(reading), self.decode_temp(reading))
+
+    def decode_alti(self, reading: bytearray) -> float:
+        pressure_pa = (reading[5] << 16 | reading[4] << 8 | reading[3]) / 64.0
         alti_m = (
             1 - (((pressure_pa / 100) / _BAROMETRIC_PRESSURE) ** 0.190284)
         ) * 44307.69396 # formula from https://www.weather.gov/media/epz/wxcalc/pressureAltitude.pdf
         return alti_m
+
+    def decode_temp(self, reading: bytearray) -> float:
+        temp_c = (reading[2] << 16 | reading[1] << 8 | reading[0]) / 65536.0
+        return temp_c
 
     @property
     def error(self) -> float:
