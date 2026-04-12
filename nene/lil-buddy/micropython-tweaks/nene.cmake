39adc83600b6154900830bf30d471a5dd81fa82f
Sam Procter <sam.procter@gmail.com>
2026-04-12 16:20:32 -0400

    Fix path in cmake file

--- nene/lil-buddy/micropython-tweaks/nene.cmake
+++ nene/lil-buddy/micropython-tweaks/nene.cmake
@@ -1,2 +1,2 @@
-include(~/git/ulab/code/micropython.cmake)
+include(~/git/micropython-ulab/code/micropython.cmake)
 include(~/git/model-rocketry/nene/lil-buddy/c-modules/hidden_buffer/micropython.cmake)
